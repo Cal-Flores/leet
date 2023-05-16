8296a31a45d766f3b642bb770fa4a3401cd39bff
calebflores <calebflores1388@gmail.com>
2023-05-15 19:54:40 -0500

    prob

--- forLoop.js
+++ forLoop.js
@@ -1,10 +1,6 @@
-let array = [20, 25, 30, 35, 40, 50]
 
-
-for (let i = 0; i <= array.length - 1; i++) {
-    let number = array[i]
-    number = number * 2
-    console.log(number)
+for (let i = 0; i < 30; i += 5) {
+    console.log(i)
 }
 
 
