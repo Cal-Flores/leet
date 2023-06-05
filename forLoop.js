bbd9df5818a2a06bb2be465a2b7536f9af90c43b
calebflores <calebflores1388@gmail.com>
2023-06-05 01:22:41 -0500

    prb2

--- forLoop.js
+++ forLoop.js
@@ -7,3 +7,4 @@ for (let i = 0; i < 30; i += 5) {
 // array.length -1 ==== 5
 // i ==== 5
 // number === 35
+// Write a method caesarCipher that takes in a string and a number.The function should return a new string where every character of the original is shifted num characters in the alphabet.
